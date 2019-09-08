2929d24aa09e2a3d317d0105ee150be7540e2fcd
ywwang-notes <51767154+ywwang-notes@users.noreply.github.com>
2019-09-08 00:12:58 -0700

    Update vox2mni.m

--- matlab/vox2mni.m
+++ matlab/vox2mni.m
@@ -1,19 +1,11 @@
 function MNI=vox2mni(M,VOX)
-
-%function MNI=vox2mni(M,VOX)
-
-%Voxel-space to MNI-space
+% Voxel-space to MNI-space
+% source: http://gibms.mc.ntu.edu.tw/bmlab/tools/data-analysis-codes/vox2mni-m/M=M(1:3,1:3);
 
 T=M(1:3,4);
-
-M=M(1:3,1:3);
-
 MNI=M*VOX;
-
 for i=1:3
-
-MNI(i,:)=MNI(i,:)+T(i);
-
+  MNI(i,:)=MNI(i,:)+T(i);
 end
 
 return
